13e113e090adb1ae60c65ab37ca7f1fbfad34e2c
tianlu1677 <tianlu1677@gmail.com>
2019-08-21 00:36:02 +0800

    pro

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,29 +1,4 @@
 import Taro from "@tarojs/taro/types/index"
-//
-// function siteConfig() {
-//   // let env = "staging";
-//   // let env = 'dev'
-//   let env = 'production'
-//   if (env === "dev") {
-//     return {
-//       identity: 'qiuliao',
-//       API_PORT: "http://localhost:4000"
-//     };
-//   }
-//   if (env === "staging") {
-//     return {
-//       identity: 'qiuliao',
-//       API_PORT: "https://fans.niubibeta.com",
-//       api_port: "https://fans.niubibeta.com",
-//     };
-//   }
-//   if (env === "pro") {
-//     return {
-//       identity: 'qiuliao',
-//       API_PORT: "https://fans.meirixinxue.com"
-//     };
-//   }
-// }
 
 export default class siteConfig {
   static identity() {
@@ -31,9 +6,8 @@ export default class siteConfig {
   }
 
   static api_port() {
-    // return "https://fans.niubibeta.com"
-    // return "https://fans.meirixinxue.com"
-    return 'https://fans.niubibeta.com'
-
+    // return "http://localhost:4000"
+    return "https://fans.meirixinxue.com"
+    // return 'https://fans.niubibeta.com'
   }
 }
