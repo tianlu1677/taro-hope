06dcaae3ec5ac2c599b73131a1a99ac6ba65caad
tianlu1677 <tianlu1677@gmail.com>
2019-08-25 12:23:55 +0800

    fix wechat login

--- src/pages/login/auth/index.weapp.js
+++ src/pages/login/auth/index.weapp.js
@@ -1,7 +1,7 @@
 import Taro, {Component} from "@tarojs/taro";
 import {View, Button} from "@tarojs/components";
 import {connect} from "@tarojs/redux";
-import {loginSystem, getSessionKey} from '@/api/user_api'
+import {getSessionKey} from '@/api/user_api'
 
 const CurrentAPPId = 'wxee56e8f240c9e89b'
 const CurrentClientType = 'wechat'
@@ -11,84 +11,49 @@ class Auth extends Component {
     addGlobalClass: true
   }
 
-  getUserInfo = async (session_key, detail) => {
-    const account_response = await loginSystem({
-      session_key: session_key,
-      encrypted_data: detail.encryptedData,
-      iv: detail.iv,
-      raw_data: detail.rawData,
-      app_id: CurrentAPPId,
-      client_type: CurrentClientType
+  onSignIn = async (event) => {
+    const { detail } = event
+
+    if(detail.errMsg !== 'getUserInfo:ok') {
+      Taro.showToast({
+        title: "请授权当前用户",
+        icon: "none",
+        duration: 500
+      });
+      return
+    }
+
+    Taro.showToast({
+      title: "登录中...",
+      icon: "none",
+      duration: 1500
     });
 
-    const user = account_response.user
+    const code_res = await Taro.login()
+    // console.log('xxxx', detail)
+    const res_login = await getSessionKey({
+      code: code_res.code,
+      app_id: CurrentAPPId,
+      client_type: CurrentClientType,
+      user_info: detail.userInfo
+    });
+    const session_key = res_login.session_key
+    // const userInfo = await Taro.getUserInfo()
+    // console.log('userinfo', userInfo)
+
+    const user = res_login.user
+    Taro.setStorageSync('session_key', session_key)
     Taro.setStorageSync("auth_token", user.auth_token);
     Taro.setStorage({key: "user_id", data: user.id});
-    Taro.setStorage({key: "user_name", data: user.name});
-    Taro.hideLoading();
+
     Taro.showToast({
       title: "登录成功",
       icon: "none",
-      duration: 500
+      duration: 800
     });
     let fromPath = Taro.getStorageSync("last_path") || "/pages/home/index";
     Taro.removeStorageSync("last_path");
     Taro.reLaunch({url: fromPath});
-  }
-
-  fetchSessionKey = async () => {
-    const code_res = await Taro.login()
-    const res_login = await getSessionKey({code: code_res.code, app_id: CurrentAPPId, client_type: CurrentClientType});
-    const session_key = res_login.session_key
-    await Taro.setStorage({key: 'session_key', data: session_key})
-    return session_key
-  }
-
-  realLogin = async (detail, relogin = false) => {
-    if(relogin) {
-      await this.fetchSessionKey()
-      let session_key = Taro.getStorageSync('session_key')
-      await this.getUserInfo(session_key, detail)
-      return
-    }
-
-    let session_key = Taro.getStorageSync('session_key')
-    if (session_key.length < 1) {
-      await this.fetchSessionKey()
-    }
-
-    await this.getUserInfo(session_key, detail)
-  }
-
-
-  onSignIn = async (event) => {
-    const {detail} = event
-    try {
-      if (detail.errMsg === 'getUserInfo:ok') {
-        Taro.checkSession({
-          success: async (sevent) => {
-            // console.log('success', sevent)
-            if(sevent.errMsg === 'checkSession:ok') {
-              await this.realLogin(detail, false)
-            } else {
-              await this.realLogin(detail, true)
-            }
-          },
-          fail: async (e) => {
-            // console.log('error', e)
-            await this.realLogin(detail, true)
-          }
-        })
-      } else {
-        Taro.showToast({
-          title: "未登录成功请重试",
-          icon: "none",
-          duration: 1000
-        });
-      }
-    }catch (e) {
-      await this.realLogin(detail, true)
-    }
   }
 
   render() {
